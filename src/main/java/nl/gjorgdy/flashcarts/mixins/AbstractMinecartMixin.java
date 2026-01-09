a8220f74d2b8092ef188d57cd93040b4f34346ec
Gjorgdy <jordy.reins@proton.me>
2026-01-09 21:58:12 +0100

    1.21.11 ADD: fast cart logic

---
+++ src/main/java/nl/gjorgdy/flashcarts/mixins/AbstractMinecartMixin.java
@@ -0,0 +1,66 @@
+package nl.gjorgdy.flashcarts.mixins;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.vehicle.VehicleEntity;
+import net.minecraft.world.entity.vehicle.minecart.*;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractMinecart.class)
+public abstract class AbstractMinecartMixin extends VehicleEntity {
+
+	@Mutable
+	@Final
+	@Shadow
+	private MinecartBehavior behavior;
+
+	@Unique
+	private final AbstractMinecart self = (AbstractMinecart) (Object) this;
+
+	@Unique
+	private final static float BLOCK_SPEED_FACTOR_RAILS = 16.0F;
+
+	public AbstractMinecartMixin(EntityType<?> entityType, Level level) {
+		super(entityType, level);
+	}
+
+	@Inject(at = @At("HEAD"), method = "getBlockSpeedFactor", cancellable = true)
+	private void _getBlockSpeedFactor(CallbackInfoReturnable<Float> cir) {
+		if (self.getBehavior() instanceof OldMinecartBehavior) return;
+		BlockState blockState = this.level().getBlockState(this.blockPosition());
+		cir.setReturnValue(blockState.is(BlockTags.RAILS) ? BLOCK_SPEED_FACTOR_RAILS : super.getBlockSpeedFactor());
+		cir.cancel();
+	}
+
+	@Inject(at = @At("HEAD"), method = "tick")
+	public void _tick(CallbackInfo ci) {
+		getPassengers().forEach(p -> {
+			if (p instanceof ServerPlayer player) {
+				var speed = getSpeedBlocksPerSecond();
+				if (speed > 0.1) { // TODO: base on breaking speed
+					player.sendSystemMessage(Component.literal(String.format("%.2f", speed) + " b/s"), true);
+				} else {
+					player.sendSystemMessage(Component.empty(), true);
+				}
+			}
+		});
+		if (self instanceof Minecart && this.behavior instanceof OldMinecartBehavior) {
+//			System.out.println("Upgrading minecart behavior to new system.");
+			this.behavior = new NewMinecartBehavior(self);
+		}
+	}
+
+	@Unique
+	private double getSpeedBlocksPerSecond() {
+		return Math.min(this.getKnownSpeed().length() * 20, 100);
+	}
+
+}
